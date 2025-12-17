f5b480e9fac5942db5cbfa6380e0641384ab51ef
cancio <cancius.condori@gmail.com>
2025-12-17 17:01:34 -0400

    transfering files without red lines

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,54 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+// import swaggerUi from 'swagger-ui-express';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+import { errorMiddleware } from '@e-com/libs';
+import authRouter from './routes/auth.route.js';
+// import swaggerDocument from './swagger/swagger-output.json' with { type: 'json' };
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+// Swagger docs
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.get('/docs-json', (req, res) => {
+//   res.json(swaggerDocument);
+//   // res.status(200).json(swaggerDocument);
+// });
+
+// Routes
+app.use('/api', authRouter);
+
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 6001;
+app.listen(port, () => {
+  console.log(`Auth Service is running on http://localhost:${port}/api`);
+  console.log(`Swagger Docs are available at http://localhost:${port}/docs`);
 });
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(err);
+  return res
+    .status(err.status || 500)
+    .json({ message: err.message || 'Internal Server Error' });
+});
+
+// server.on("error", (err) => {
+//   console.log("Server Error:", err);
+// });
