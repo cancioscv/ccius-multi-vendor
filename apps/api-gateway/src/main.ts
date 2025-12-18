66906283a79fbfd7531a0a6da7c2cb2d58a8772e
cancio <cancius.condori@gmail.com>
2025-12-18 10:25:57 -0400

    auth-service and api-gateway running

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,47 @@
+import express from "express";
+import cors from "cors";
+import proxy from "express-http-proxy";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+// import swaggerUi from "swagger-ui-express";
+// import axios from "axios";
+import cookirParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
+
+app.use(morgan("dev"));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(cookirParser());
+
+app.set("trus proxy", 1);
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: (req: any) => (req.user ? 1000 : 100),
+  message: { error: "Too many requests form this IP, please try again later" },
+  standardHeaders: true,
+  legacyHeaders: true,
+  // keyGenerator: (req: any) => req.ip, // The keyGenerator is implicit and uses req.ip by default
+});
+
+app.use(apiLimiter);
+app.get("/gateway-health", (req, res) => {
+  res.send({ message: "Welcome to api-gateway!" });
+});
+
+app.use("/", proxy("http://localhost:6001"));
+
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+  console.log(`Api Gateway running at http://localhost:${port}/api`);
+});
+server.on("error", console.error);
